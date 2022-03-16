658547fc9853d24221a62933155f5e25bda2edfd
sergio0623 <101436007+sergio0623@users.noreply.github.com>
2022-03-15 21:16:52 -0500

    parcial

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,11 @@
-print("hola")
+import random
+
+numero = random.randint(1,100) 
+contador = 0
+print("los divisores de ",numero)
+for divisor in range(1,numero+1):
+    if (numero % divisor) == 0 :
+        print(divisor,"es divisor")
+        contador += 1
+print("el numero ",numero," tiene ",contador," divisores")
+print("el numero ",numero,"si es inteligente")
